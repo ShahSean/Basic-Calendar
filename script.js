e7b13322d4c1bd7daef3b4bfab4d61e7cfd4ebaa
Shahryar Shagoshtasbi <shahryar9782@gmail.com>
2020-06-16 14:38:35 -0400

    Some modification, ToDo List Initialized

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ var darkBtn = document
   .addEventListener("click", darkModeFun);
 
 function darkModeFun() {
-  let element = document.querySelector(".content");
-  element.classList.toggle("dark-theme");
+  let darkContent = document.querySelector(".light-theme-content");
+  let darkBody = document.querySelector(".light-theme-body");
+  let darkNav = document.querySelector(".light-theme-nav");
+
+  darkContent.classList.toggle("dark-theme-content");
+  darkBody.classList.toggle("dark-theme-body");
+  darkNav.classList.toggle("dark-theme-nav");
 }
