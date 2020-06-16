53a8642206ff33af486829c89c5b72d7061139ed
Shahryar Shagoshtasbi <shahryar9782@gmail.com>
2020-06-15 23:34:36 -0400

    Failed to toggle between classes

--- script.js
+++ script.js
@@ -5,5 +5,10 @@ var darkModeBtn = document
 function darkMode() {
   //   console.log(document.getElementsByClassName("test-class"));
   //   alert("This is darker");
-  document.body.style.backgroundImage = "url('./img/bckg2.jpg')";
+  //   document.body.style.backgroundColor = "red";
+  // "rgb(" + [44, 34, 34, 0.96].join(",") + ")";
+  //   document.querySelector(".content").style.backgroundColor =
+  //     "rgb(" + [44, 34, 34, 0.56].join(",") + ")";
+  const div = document.createElement("div");
+  div.classList.toggle("dark-theme");
 }
