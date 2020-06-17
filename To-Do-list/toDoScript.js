4abca918d643b1964ca0aa5b20cb9735c29da19d
Shahryar Shagoshtasbi <shahryar9782@gmail.com>
2020-06-17 12:39:28 -0400

    Staging

--- To-Do-list/toDoScript.js
+++ To-Do-list/toDoScript.js
@@ -1,5 +1,6 @@
 var usrInput = document.getElementById("usr-input");
 let subBtn = document.querySelector("#submit-btn");
+let i = 0;
 
 function addBtnClickHandler() {
   addTask(usrInput.value);
@@ -7,10 +8,28 @@ function addBtnClickHandler() {
 subBtn.addEventListener("click", addBtnClickHandler);
 
 function addTask(text) {
-  let taskBox = document.createElement("ul");
   let newTask = document.createElement("li");
-  newTask.style.border = "2px solid blue";
-  taskBox.appendChild(newTask);
+  let delBtn = document.createElement("button");
+
+  newTask.style.listStyleType = "none";
+  newTask.style.border = "1px solid grey";
+  newTask.style.borderRadius = "10%";
+
+  delBtn.style.margin = "2px 6px";
+  delBtn.appendChild(document.createTextNode("Delete"));
+  delBtn.classList.add("del-btn-" + (i = i + 1));
+  delBtn.addEventListener("click", removeHandler(delBtn.classList));
+
   newTask.appendChild(document.createTextNode(text));
-  document.querySelector("body > div").appendChild(taskBox);
+  newTask.appendChild(delBtn);
+
+  document.querySelector("body > div").appendChild(newTask);
 }
+
+function removeHandler(delBtn) {
+  // let delBtnHandler = document.querySelector("[class^='del-btn-'");
+  console.log(delBtn);
+}
+
+// Delete Button
+// Storing them in hard for further access.
