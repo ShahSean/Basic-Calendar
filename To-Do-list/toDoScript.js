112101991cab21681e3f453c95a5f5f48291628f
Shahryar Shagoshtasbi <shahryar9782@gmail.com>
2020-06-16 22:20:38 -0400

    changed divs to Li

--- To-Do-list/toDoScript.js
+++ To-Do-list/toDoScript.js
@@ -1,12 +1,14 @@
-const usrInput = document.querySelector("#usr-input").value;
+var usrInput = document.getElementById("usr-input").value;
 let subBtn = document
   .querySelector("#submit-btn")
-  .addEventListener("click", addTask(usrInput));
+  .addEventListener("click", addTask);
 
-function addTask(task) {
-  const taskBox = document.createElement("div");
-  const newTask = document.createElement("div");
+function addTask(e) {
+  e.preventDefault();
+  let taskBox = document.createElement("ul");
+  let newTask = document.createElement("li");
+  newTask.style.border = "2px solid blue";
   taskBox.appendChild(newTask);
+  newTask.appendChild(document.createTextNode(usrInput));
   document.querySelector("body > div > div").appendChild(taskBox);
-  console.log("input is" + task);
 }
