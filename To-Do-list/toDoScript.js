3c1fccec34905d1485f0e4e81e74c4cffd05548a
Shahryar Shagoshtasbi <shahryar9782@gmail.com>
2020-06-18 11:00:07 -0400

    delete button deletes itself !

--- To-Do-list/toDoScript.js
+++ To-Do-list/toDoScript.js
@@ -1,15 +1,19 @@
 var usrInput = document.getElementById("usr-input");
 let subBtn = document.querySelector("#submit-btn");
+
+// Submit Button Event Listener
+subBtn.addEventListener("click", addBtnClickHandler);
+
 let i = 0;
 
 function addBtnClickHandler() {
   addTask(usrInput.value);
 }
-subBtn.addEventListener("click", addBtnClickHandler);
 
 function addTask(text) {
   let newTask = document.createElement("li");
   let delBtn = document.createElement("button");
+  delBtn.addEventListener("click", removeHandler);
 
   newTask.style.listStyleType = "none";
   newTask.style.border = "1px solid grey";
@@ -18,7 +22,6 @@ function addTask(text) {
   delBtn.style.margin = "2px 6px";
   delBtn.appendChild(document.createTextNode("Delete"));
   delBtn.classList.add("del-btn-" + (i = i + 1));
-  delBtn.addEventListener("click", removeHandler(delBtn.classList));
 
   newTask.appendChild(document.createTextNode(text));
   newTask.appendChild(delBtn);
@@ -26,10 +29,15 @@ function addTask(text) {
   document.querySelector("body > div").appendChild(newTask);
 }
 
-function removeHandler(delBtn) {
-  // let delBtnHandler = document.querySelector("[class^='del-btn-'");
-  console.log(delBtn);
+function removeHandler(e) {
+  if (confirm("Are you sure ?!")) {
+    var elem = e.target.parentElement;
+    elem.removeChild(e.target);
+    // console.log("the task " + e);
+  } else {
+    console.log("Not Sure");
+  }
 }
 
 // Delete Button
-// Storing them in hard for further access.
+// Storing them in local storage for further access.
